7e5253211557efb5238fc75a6e276859fc16dcc2
Rob DeRosa <rob@Robs-MacBook-Pro.local>
2016-10-13 12:09:11 -0600

    readme updated

--- Sport.Mobile.Shared/Keys.cs
+++ Sport.Mobile.Shared/Keys.cs
@@ -3,7 +3,7 @@
 	public class Keys
 	{
 		public static readonly string SourceCodeUrl = "https://github.com/xamarin/sport";
-		public static readonly string GooglePushNotificationSenderId = "643221174110";
+		public static readonly string GooglePushNotificationSenderId = "REPLACE_WITH_YOUR_GOOGLE_SENDER_ID_FOR_PUSH_NOTIFICATIONS";
 		public static readonly string HockeyAppId_Android = "REPLACE_WITH_YOUR_HOCKEYAPP_KEY";
 		public static readonly string HockeyAppId_iOS = "REPLACE_WITH_YOUR_HOCKEYAPP_KEY";
 
