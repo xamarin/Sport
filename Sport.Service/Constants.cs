4aa0bbd6b339145f8e395261717520530078a837
JB69 <jasonbird69@gmail.com>
2017-01-17 15:44:00 +1000

    added new connection string

--- Sport.Service/Constants.cs
+++ Sport.Service/Constants.cs
@@ -3,6 +3,7 @@ namespace Sport.Service
 {
 	internal class Constants
 	{
+		//Endpoint=sb://jb69sporthubnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=w/oGRHOl4CojQuSehOyXcZlOlDGNfaetlJVQHK0+euI=
 		internal static readonly string HubConnectionString = "Endpoint=sb://xamarinsporthubnamespace.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=TF6GC3POPu3WnxZxKdv520ITMZ3rh7Lmj58STHpMuow=";
 		internal static readonly string HubName = "XamarinSportNotificationHub";
 	}
