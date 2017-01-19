17cfd8337d12117a8be78d8f1d9dc2f7995d7ef4
Jason <jasonbird69@gmail.com>
2017-01-19 13:52:52 +1000

    working with facebook login and jb69-sport azure backend

--- Sport.Service/Controllers/AuthenticationController.cs
+++ Sport.Service/Controllers/AuthenticationController.cs
@@ -12,17 +12,17 @@ using System.Web.Http;
 namespace Sport.Service.Controllers
 {
     [Authorize]
-	public class AuthenticationController : TableController<Athlete>
-	{
-		MobileServiceContext _context = new MobileServiceContext();
+    public class AuthenticationController : TableController<Athlete>
+    {
+        MobileServiceContext _context = new MobileServiceContext();
 
-		[HttpGet, Route("api/getUserIdentity")]
-		public async Task<GoogleCredentials> GetUserIdentity(HttpRequestMessage request = null)
-		{
+        [HttpGet, Route("api/getUserIdentity")]
+        public async Task<GoogleCredentials> GetUserIdentity(HttpRequestMessage request = null)
+        {
             try
             {
-				var cp = User as ClaimsPrincipal;
-				var id = cp.FindFirst(ClaimTypes.NameIdentifier);
+                var cp = User as ClaimsPrincipal;
+                var id = cp.FindFirst(ClaimTypes.NameIdentifier);
                 var creds = await User.GetAppServiceIdentityAsync<GoogleCredentials>(request ?? Request);
                 return creds;
             }
@@ -32,106 +32,59 @@ namespace Sport.Service.Controllers
             }
         }
 
-		string _userId;
-		public string UserId
-		{
-			get
-			{
-				if (_userId == null) { }
-				{
-					var claimsUser = (ClaimsPrincipal)User;
-					var id = claimsUser?.FindFirst(ClaimTypes.NameIdentifier);
-					if(id != null)
-						_userId = id.Value;
-				}
-
-				return _userId;
-			}
-		}
-
-		public bool IsCurrentUser(Athlete athlete)
-		{
-			if(athlete == null)
-				return false;
-
-			return athlete.UserId == UserId;
-		}
-		public void EnsureHasPermission(Athlete athlete, HttpRequestMessage request)
-		{
-			EnsureHasPermission(new Athlete[] { athlete }, request);
-		}
-
-		public void EnsureHasPermission(Athlete[] athletes, HttpRequestMessage request)
-		{
-			foreach(var a in athletes)
-			{
-				if(a != null && a.UserId == UserId)
-					return;
-			}
-
-			throw "Invalid permission".ToException(request);
-		}
-
-		public void EnsureAdmin(HttpRequestMessage request)
-		{
-			if(UserId != null)
-			{
-				var isAdmin = _context.Athletes.SingleOrDefault(a => a.UserId == UserId && a.IsAdmin) != null;
-
-				if(isAdmin)
-					return;
-			}
-
-			throw "Invalid permission".ToException(request);
-		}
-		
-	[AuthorizeLevel(AuthorizationLevel.User)]
-    public async Task<JObject> GetUserInfo()
-    {
-        //Get the current logged in user
-        ServiceUser user = this.User as ServiceUser;
-        if (user == null)
+        string _userId;
+        public string UserId
         {
-            throw new InvalidOperationException("This can only be called by authenticated clients");
+            get
+            {
+                if (_userId == null) { }
+                {
+                    var claimsUser = (ClaimsPrincipal)User;
+                    var id = claimsUser?.FindFirst(ClaimTypes.NameIdentifier);
+                    if (id != null)
+                        _userId = id.Value;
+                }
+
+                return _userId;
+            }
         }
 
-        //Get Identity Information for the current logged in user
-        var identities = await user.GetIdentitiesAsync();
-        var result = new JObject();
-
-        //Check if the user has logged in using Facebook as Identity provider
-        var fb = identities.OfType<FacebookCredentials>().FirstOrDefault();
-        if (fb != null)
+        public bool IsCurrentUser(Athlete athlete)
         {
-            var accessToken = fb.AccessToken;
-            result.Add("facebook", await GetProviderInfo("https://graph.facebook.com/me?access_token=" + accessToken));
+            if (athlete == null)
+                return false;
+
+            return athlete.UserId == UserId;
+        }
+        public void EnsureHasPermission(Athlete athlete, HttpRequestMessage request)
+        {
+            EnsureHasPermission(new Athlete[] { athlete }, request);
         }
 
-        //Check if the user has logged in using Microsoft Identity provider
-        var ms = identities.OfType<MicrosoftAccountCredentials>().FirstOrDefault();
-        if (ms != null)
+        public void EnsureHasPermission(Athlete[] athletes, HttpRequestMessage request)
         {
-            var accessToken = ms.AccessToken;
-            result.Add("microsoft", await GetProviderInfo("https://apis.live.net/v5.0/me/?method=GET&access_token=" + accessToken));
+            foreach (var a in athletes)
+            {
+                if (a != null && a.UserId == UserId)
+                    return;
+            }
+
+            throw "Invalid permission".ToException(request);
         }
 
-        //Check if the user has logged in using Google as Identity provider
-        var google = identities.OfType<GoogleCredentials>().FirstOrDefault();
-        if (google != null)
+        public void EnsureAdmin(HttpRequestMessage request)
         {
-            var accessToken = google.AccessToken;
-            result.Add("google", await GetProviderInfo("https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + accessToken));
+            if (UserId != null)
+            {
+                var isAdmin = _context.Athletes.SingleOrDefault(a => a.UserId == UserId && a.IsAdmin) != null;
+
+                if (isAdmin)
+                    return;
+            }
+
+            throw "Invalid permission".ToException(request);
         }
 
-        return result;
+
     }
-
-    private async Task<JToken> GetProviderInfo(string url)
-    {
-        var c = new HttpClient();
-        var resp = await c.GetAsync(url);
-        resp.EnsureSuccessStatusCode();
-        return JToken.Parse(await resp.Content.ReadAsStringAsync());
-    }
-}
 }
